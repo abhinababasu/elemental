cc4a859c459b4914cae7e0c4b996679d20469eb3
Abhinaba Basu <abhinab@microsoft.com>
2019-03-04 12:37:49 -0800

    clean up

--- wordelement/build.sh
+++ wordelement/build.sh
@@ -1,14 +1,17 @@
+#!/bin/bash
 
 VERSION=0.1
 CONTAINER=elementservice
+
 echo Build go sources
 go build ./wordelementservice.go
-echo Building image $CONTAINER:$VERSION
+
+echo Building Image $CONTAINER:$VERSION
 docker build -t $CONTAINER:$VERSION .
 docker tag $CONTAINER:$VERSION $DOCKER_ID_USER/$CONTAINER
-docker tag $CONTAINER:$VERSION bonggeek.azurecr.io/$CONTAINER
 
-echo Done .............................
+echo Done ................................
 echo 
-echo Run container using
+echo Run Container using
 echo docker run -d -p 8080:8080 $CONTAINER:$VERSION
+
